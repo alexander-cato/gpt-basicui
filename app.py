89aa666d5349aa9b9c724ed3421ef88b1ee89348
Alexander Cato <52341592+alexander-cato@users.noreply.github.com>
2023-05-15 14:15:05 -0500

    fix rule sanitization

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 import os
 from flask import Flask, request, jsonify, render_template
+from urllib.parse import quote
 from genie import get_chatgpt_response
 
 app = Flask(__name__)
 
 current_model = "gpt-4"
+
+def escape_html(unsafe):
+    return (unsafe.replace("&", "&amp;")
+                  .replace("<", "&lt;")
+                  .replace(">", "&gt;")
+                  .replace('"', "&quot;")
+                  .replace("'", "&#039;"))
+
+def sanitize_user_input(input):
+    return (input.replace("\r\n", "\\n")
+                  .replace("\n", "\\n")
+                  .replace("\r", "\\n")
+                  .replace("\\", "\\\\")
+                  .replace(",", "\\,")
+                  .replace(":", "\\:")
+                  .replace("{", "\\{")
+                  .replace("}", "\\}")
+                  .replace("[", "\\[")
+                  .replace("]", "\\]"))
+
+def sanitize_input(input):
+    return quote(input)
 
 @app.route("/")
 def home():
@@ -13,6 +36,8 @@ def home():
 @app.route("/api/chatgpt", methods=["POST"])
 def chatgpt():
     messages = request.json.get("messages", [])
+    rules = get_rules()
+    messages.insert(0, {'role': 'system', 'content': rules})
     if not messages:
         return jsonify({"error": "Missing messages"}), 400
 
@@ -33,11 +58,13 @@ def set_model():
         return jsonify({"status": "Model changed"}), 200
     return jsonify({"error": "No model specified"}), 400
 
-@app.route("/api/get_rules", methods=["GET"])
 def get_rules():
     with open("rules.txt", "r") as file:
         rules = file.read()
-    return jsonify({"rules": rules})
+    escaped_rules = escape_html(rules)
+    sanitized_rules = sanitize_user_input(escaped_rules)
+    final_sanitized_rules = sanitize_input(sanitized_rules)
+    return final_sanitized_rules
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6565)
